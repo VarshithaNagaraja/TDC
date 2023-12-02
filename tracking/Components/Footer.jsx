fd0e62e0908eeb0507c563c7b87f27eafac53c15
VarshithaNagaraja <144626018+VarshithaNagaraja@users.noreply.github.com>
2023-12-01 21:31:48 -0800

    Add files via upload

---
+++ tracking/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="mt-10 py-10 border-t text-center">
+          <p>© 2023 Team AVH. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
